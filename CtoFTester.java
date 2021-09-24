f39942291b037b1894c0425085efaa78a85a5c62
jennifer <jjjennifer411@gmail.com>
2021-09-23 23:52:25 -0400

    wrote methods :D

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,10 @@
-celsiusToFahrenheit()
+public class CtoFTester {
+  public static void celsiusToFahrenheit(double celsius) {
+    double farenheit = (9.0/5.0)*celsius + 32;
+    System.out.println(farenheit);
+  }
+  public static void farenheitToCelsius(double farenheit) {
+    double celsius = (5.0/9.0)*farenheit - 32;
+    System.out.println(celsius);
+  }
+}
